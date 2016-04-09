fdbd6db3ae4bdde0ed779507b1edceb6c8c6f2a5
Francisco Martinez <martinez.francisco.842@gmail.com>
2016-04-08 17:14:33 -0700

    two classes didnt get pushed

---
+++ app/src/main/java/falconrobotics/scoutingprogram/Interface_Spinners.java
@@ -0,0 +1,94 @@
+package falconrobotics.scoutingprogram;
+
+/**
+ * Created on 2/25/2016.
+ */
+public interface
+        Interface_Spinners {
+        String[] NoReachCross =
+                {
+                        "NO",
+                        "REACH",
+                        "CROSS"
+                };
+        String[] NoTryYes =
+                {
+                        "NO",
+                        "TRY",
+                        "YES"
+                };
+        String[] NoYellowRed =
+                {
+                        "NO",
+                        "YELLOW",
+                        "RED"
+                };
+        String[] NoAutoTeleBoth =
+                {
+                        "NO",
+                        "AUTO",
+                        "TELE",
+                        "BOTH"
+                };
+        String[] NoYes =
+                {
+                        "NO",
+                        "YES"
+                };
+        String[] ShooterType =
+                {
+                        "UNKNOWN",
+                        "NONE",
+                        "WHEELS",
+                        "CATAPULT",
+                        "TURRET",
+                        "SLINGSHOT",
+                        "OTHER"
+                };
+        String[] ShootingType =
+                {
+                        "LOW",
+                        "BOTH",
+                        "HIGH"
+                };
+        String[] Weight =
+                {
+                        "UNKNOWN",
+                        "SUB 50",
+                        "50-59",
+                        "60-69",
+                        "70-79",
+                        "80-89",
+                        "90-99",
+                        "100-109",
+                        "110-119",
+                        "120"
+                };
+        String[] DrivetrainType =
+                {
+                        "TRACTION",
+                        "PNEUMATIC",
+                        "MECHANUM",
+                        "SWERVE",
+                        "OMNI",
+                        "TANK",
+                        "HYBRID"
+                };
+        String[] EXPERIENCE =
+                {
+                        "First",
+                        "Second",
+                        "Third",
+                        "Fourth",
+                        "Fifth (VEX/FLL)",
+                        "Sixth (VEX/FLL)",
+                        "Seventh (VEX/FLL)"
+                };
+        String[] ClimbSpeed =
+                {
+                        "< 10",
+                        "< 20",
+                        "< 30",
+                        "< 40",
+                };
+}
